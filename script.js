653d8cf6bff694d5f423ae9e3b89e8f9a6c51d79
ploylapas <lapasrada_k@cmu.ac.th>
2021-07-25 19:34:17 +0700

    almost

--- script.js
+++ script.js
@@ -1,69 +1,140 @@
-const inputBox = document.querySelector(".inputText input")
-const addButton = document.querySelector(".inputText button")
-const todolist = document.querySelector(".todolist")
-const clearAll = document.querySelector(".clearAll button")
+const cout = console.log
+// getlocalStorage
+let localData = JSON.parse(localStorage.getItem("userData"))
+if (localStorage.getItem('userData') == null) {
+    const dataformat = {
+        incompleteArr: [], completeArr: []
+    }
+    localStorage.setItem('userData', JSON.stringify(dataformat))
+}
 
-inputBox.onkeyup = ()=>{
-    let Userdata  = inputBox.value;
-    if(Userdata.trim() != 0){
-        addButton.classList.add("active");
-    }else{
-        addButton.classList.remove("active");
+//click&enter alert
+function coutAlert(input) {
+    if (input.length == 0 || input.indexOf(' ') == 0) {
+        document.getElementById("TaskInput").value = ""
+        alert("CANNOT BE EMPTY")
+        return false
+    }
+    return true
+}
+function keypress(ev){
+    if (ev.keyCode === 13) 
+        EnteringInput()
+}
+function empty(input){
+    if (input.length == 0 || input.indexOf(' ') == 0) {
+        return false
+    }
+    return true
+}
+
+function ArrIndex(arr, key) {
+    for (let j = 0; j < arr.length; ++j) {
+        if (arr[j] === key) 
+            return j
+    }
+    return -1
+}
+
+// function addTodo&addtoDone
+function addTodo(inputText){
+    const input = inputText;
+    const newTask = document.createElement('p')
+    const boxTask = document.createElement('div')
+    const blockBTn = document.createElement('div')
+
+    function DonePress(){
+        newTask.classList.add("todone")
+        document.getElementById("doneAll").prepend(newTask)
+        localData.incompleteArr.splice(localData.incompleteArr.indexOf(input),1)
+        localData.completeArr.push(input)
+        localStorage.setItem('userData', JSON.stringify(localData))
+        boxTask.remove()
+    }
+
+    function BinPress(){
+        localData.incompleteArr.splice(localData.incompleteArr.indexOf(input),1)
+        localStorage.setItem('userData', JSON.stringify(localData))
+        boxTask.remove()
+    }
+
+    boxTask.classList.add("todo")
+    
+    const doneBtn = document.createElement('button')
+    const binBtn = document.createElement('button')
+    newTask.innerHTML = inputText
+    doneBtn.innerHTML = '<i class="fas fa-check"></i>';
+    binBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    doneBtn.classList.add("pressDoneBtn");
+    binBtn.classList.add("pressBinBtn");
+    doneBtn.style.visibility = "hidden"
+    binBtn.style.visibility = "hidden"
+
+    //control Mouse
+    boxTask.addEventListener('mouseover',()=>{
+        doneBtn.style.visibility = "visible";
+        binBtn.style.visibility = "visible";
+    })
+    boxTask.addEventListener('mouseout',()=>{
+        doneBtn.style.visibility = "hidden";
+        binBtn.style.visibility = "hidden";
+    })
+    doneBtn.addEventListener("click", DonePress)
+    binBtn.addEventListener("click", BinPress)
+
+    //append data
+    boxTask.append(newTask)
+    boxTask.append(blockBTn)
+    blockBTn.append(doneBtn)
+    blockBTn.append(binBtn)
+    document.getElementById("dolist").prepend(boxTask)
+
+    //clear inputBox after input
+    inputText=''
+}
+
+function addToDone(inputDone){
+    const newTask = document.createElement('p')
+    const boxTask = document.createElement('div')
+    newTask.classList.add("todone")
+    newTask.innerHTML = inputDone
+    boxTask.append(newTask)
+    document.getElementById("doneAll").prepend(boxTask)
+    newTask.classList.add("todone")
+}
+
+
+function EnteringInput(){
+    cout("")
+    const taskIn = document.getElementById("TaskInput").value
+
+    if (coutAlert(taskIn)) {
+        addTodo(taskIn)
+
+        localData.incompleteArr.push(taskIn)
+        localStorage.setItem('userData', JSON.stringify(localData))
+        document.getElementById("TaskInput").value = ""
     }
 }
-showList();
 
-//add list
-addButton.onclick =() =>{
-    let arr =[];
-    let Userdata  = inputBox.value;
-    let getLocalStorage = localStorage.getItem("List Todo");
-    if(getLocalStorage == null){
-        arr =[];
-    }else{
-        arr = JSON.parse(getLocalStorage);
+
+//keep localStorage
+localData.incompleteArr.map(x => {
+    if (empty(x)) {
+        addTodo(x)
     }
-    arr.push(Userdata)
-    localStorage.setItem("List Todo", JSON.stringify(arr));
-    showList();
-}
-
-function showList(){
-    let getLocalStorage = localStorage.getItem("List Todo");
-    if(getLocalStorage == null){
-        arr =[];
-    }else{
-        arr = JSON.parse(getLocalStorage);
+})
+localData.completeArr.map(x => {
+    if (empty(x)) {
+        addToDone(x)
     }
-    const count =arr.length;
-    const notDoneNum = document.querySelector(".notDoneNum");
-    notDoneNum.textContent = count;
+})
 
-    if(arr.length>0){
-        clearAll.classList.add("active");
-    }else{
-        clearAll.classList.remove("active");
-    }
-    let newline = '';
-    arr.forEach((element,index)=> {
-        newline += `<li> ${element} <span onclick="deleteTask(${index})";><i class="fas fa-trash"></i></span></li>`;
-    });
-    todolist.innerHTML = newline;
-    inputBox.value ="";
-}
-
-function deleteTask(index){
-    let getLocalStorage = localStorage.getItem("List Todo");
-    arr = JSON.parse(getLocalStorage);
-    arr.splice(index,1);
-    localStorage.setItem("List Todo", JSON.stringify(arr));
-    showList();
-}
-
-clearAll.onclick = ()=>{
-    arr =[];
-    localStorage.setItem("List Todo", JSON.stringify(arr));
-    showList();
-
-}
+//clearAll Click
+// const clearAll = document.querySelector(".clearAll button")
+// clearAll.addEventListener('click', () =>{
+//     window.localStorage.clear();
+//     document.getElementById("doneAll").reset();
+    
+// })
 
